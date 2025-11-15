9a0b4352491bc1e3ed1177ae6e0c1487f64bb371
JetroSulthan <jetrosulthan9626@gmail.com>
2025-11-15 19:19:15 +0700

    refactor margin button

--- pages/app/vote.vue
+++ pages/app/vote.vue
@@ -128,7 +128,7 @@
               <!-- BUTTON KIRIM -->
               <div class="flex justify-center">
                 <AppButton
-                  class="border text-white border-primary-800 flex items-center"
+                  class="border text-white mx-auto border-primary-800 flex items-center"
                   :disabled="selectedOption == -1"
                   @click="submitVote"
                   variant="secondary"
